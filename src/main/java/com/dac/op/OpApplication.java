d0a66ca459ceefc358c5f41308879f328791283e
anil ingle <anil.ingale@weboniselab.com>
2019-01-29 12:21:27 +0530

    project added 20190129

---
+++ src/main/java/com/dac/op/OpApplication.java
@@ -0,0 +1,14 @@
+package com.dac.op;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpApplication.class, args);
+	}
+
+}
+
